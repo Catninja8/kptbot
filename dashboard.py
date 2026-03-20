16ef043dadcf5458ee1003dc05065ea4379122b9
Catninja8 <Catninjacreator@gmail.com>
2026-03-20 17:22:33 +0000

    Fix indentation for app run configuration

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for
+from flask import Flask, render_template, request, jsonify
 import json
 import os
 
@@ -62,5 +62,5 @@ def get_stats():
     })
 
 if __name__ == '__main__':
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
