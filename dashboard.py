504b905e119124d94bb9c27031e7917c29521db1
Catninja8 <Catninjacreator@gmail.com>
2026-03-21 17:40:40 +0000

    Specify template folder for Flask app

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import json, os, datetime
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.')
 DATA_DIR = 'data'
 
 def load_json(path):
